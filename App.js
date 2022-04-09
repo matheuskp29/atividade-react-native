a5205ae88ec23a32d1500cf21be9de6561786772
caique <caiquetrivino@hotmail.com>
2022-04-08 22:18:49 -0300

    Criando os numeros aleatorios

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, { useState } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      n1: Math.floor(Math.random() * 61),
+      n2: Math.floor(Math.random() * 61),
+      n3: Math.floor(Math.random() * 61),
+      n4: Math.floor(Math.random() * 61),
+      n5: Math.floor(Math.random() * 61),
+      n6: Math.floor(Math.random() * 61),
+    }
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>{[this.state.n1, "\n" + this.state.n2,
+         "\n" + this.state.n3, "\n" + this.state.n4,
+         "\n" + this.state.n5,  "\n" + this.state.n6]}</Text>
+        <Button 
+          title="OK"
+          onPress={() => this.setState({
+            n1: Math.floor(Math.random() * 61),
+            n2: Math.floor(Math.random() * 61),
+            n3: Math.floor(Math.random() * 61),
+            n4: Math.floor(Math.random() * 61),
+            n5: Math.floor(Math.random() * 61),
+            n6: Math.floor(Math.random() * 61)
+          })}/>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +43,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+
